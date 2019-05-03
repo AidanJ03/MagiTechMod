c8bf0790596fc25399c8565e2c90628868c7dd9d
AidanJ03 <50225616+AidanJ03@users.noreply.github.com>
2019-05-02 21:02:23 -0700

    Add files via upload

---
+++ main/java/aidanj03/magitechmod/Lumens.java
@@ -0,0 +1,9 @@
+package aidanj03.magitechmod;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.energy.EnergyStorage;
+
+public class Lumens extends EnergyStorage
+{
+	
+}
---
+++ main/java/aidanj03/magitechmod/lists/ItemList.java
@@ -0,0 +1,16 @@
+package aidanj03.magitechmod.lists;
+
+import net.minecraft.item.Item;
+
+public class ItemList 
+{
+	public static Item bauxite_ore;
+	public static Item copper_ore;
+	
+	public static Item aluminum_ingot;
+	public static Item alumentium_ingot;
+	public static Item augsteel;
+	public static Item steel;
+	public static Item copper_ingot;
+	public static Item plasma_blaster;
+}
